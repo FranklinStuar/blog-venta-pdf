9e2eb76dffd14cbe052f0c2cb0b61444f1ba67c0
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2017-07-10 18:37:05 -0500

    Se arreglo la vistas que vienen por defecto en auth de laravel se corrigio error en el archivo mail en config/mail

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,67 +1,69 @@
-@extends('layouts.app')
+@extends('corporate.layout')
 
-@section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Login</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
-                        {{ csrf_field() }}
+@section('container')
 
-                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
-                            <label for="username" class="col-md-4 control-label">Nombre de usuario</label>
+<div class="container form-init-center">
+    <div class="panel panel-default">
+        <div class="panel-heading">Iniciar Sesión</div>
+        <div class="panel-body">
+            <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                {{ csrf_field() }}
 
-                            <div class="col-md-6">
-                                <input id="username" type="username" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
+                <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                    <label for="username" class="col-md-12 control-label">Nombre de usuario</label>
 
-                                @if ($errors->has('username'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('username') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
+                    <div class="col-md-12">
+                        <input id="username" type="username" class="form-control" name="username" value="{{ old('username') }}" required autofocus>
 
-                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Contraseña</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <div class="checkbox">
-                                    <label>
-                                        <input type="checkbox" checked name="remember" {{ old('remember') ? 'checked' : '' }}> Recuerdame
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-8 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Iniciar Sesion
-                                </button>
-
-                                <a class="btn btn-link" href="{{ route('password.request') }}">
-                                    ¿Olvidó su contraseña?
-                                </a>
-                            </div>
-                        </div>
-                    </form>
+                        @if ($errors->has('username'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('username') }}</strong>
+                            </span>
+                        @endif
+                    </div>
                 </div>
-            </div>
+
+                <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                    <label for="password" class="col-md-12 control-label">Contraseña</label>
+
+                    <div class="col-md-12">
+                        <input id="password" type="password" class="form-control" name="password" required>
+
+                        @if ($errors->has('password'))
+                            <span class="help-block">
+                                <strong>{{ $errors->first('password') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-12 col-md-offset-4">
+                        <div class="checkbox">
+                            <label>
+                                <input type="checkbox" checked name="remember" {{ old('remember') ? 'checked' : '' }}> Recuerdame
+                            </label>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="form-group">
+                    <div class="col-md-8 col-md-offset-4">
+                        <button type="submit" class="btn btn-primary">
+                            Iniciar Sesion
+                        </button>
+
+                        <a class="btn btn-link btn-dark-green" href="{{ route('password.request') }}">
+                            ¿Olvidó su contraseña?
+                        </a>
+                        <br>
+                        <a class="btn btn-link btn-light-green" href="{{ route('register') }}">
+                            Aún no tengo cuenta
+                        </a>
+                        <br>
+                    </div>
+                </div>
+            </form>
         </div>
     </div>
 </div>
