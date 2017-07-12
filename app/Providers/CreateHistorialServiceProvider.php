2a754d0330613373921293d3e1825e7806f4460a
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2017-07-12 16:45:04 -0500

    agregado imagen principal a los posts como background para el titulo agregado comentarios para las publicaciones

--- app/Providers/CreateHistorialServiceProvider.php
+++ app/Providers/CreateHistorialServiceProvider.php
@@ -17,81 +17,81 @@ class CreateHistorialServiceProvider extends ServiceProvider
      */
     public function boot(Request $request)
     {
-        // $superadmin = false;
-        // View::composer('*', function ($view) {
-        //     if (\Shinobi::isRole('superadmin')) {
-        //         $superadmin = true;
-        //     }
-        // });
+        $superadmin = false;
+        View::composer('*', function ($view) {
+            if (\Shinobi::isRole('superadmin')) {
+                $superadmin = true;
+            }
+        });
 
-        // if(!$superadmin){
+        if(!$superadmin){
 
-        //     $historial = \App\Historial::create([
-        //         'user_agent'=>$request->server()['HTTP_USER_AGENT'],
-        //         'languaje'=>$request->server()['HTTP_ACCEPT_LANGUAGE'],
-        //         'path'=>$request->url(),
-        //         'ip'=>$request->ip(),
-        //         'created_at'=>\Carbon\Carbon::now(),
-        //     ]);
+            $historial = \App\Historial::create([
+                'user_agent'=>$request->server()['HTTP_USER_AGENT'],
+                'languaje'=>$request->server()['HTTP_ACCEPT_LANGUAGE'],
+                'path'=>$request->url(),
+                'ip'=>$request->ip(),
+                'created_at'=>\Carbon\Carbon::now(),
+            ]);
             
-        //     View::composer('*', function ($view) use($historial) {
-        //         $historial->update(['user_id'=>(\Auth::user())?\Auth::user()->id:null,]);
-        //     });
+            View::composer('*', function ($view) use($historial) {
+                $historial->update(['user_id'=>(\Auth::user())?\Auth::user()->id:null,]);
+            });
             
-        //     View::composer('klorofil.*', function ($view) {
-        //         if (!\Shinobi::can('dashboard.admin')) {
-        //             abort(404);
-        //         }
-        //     });
+            View::composer('klorofil.*', function ($view) {
+                if (!\Shinobi::can('dashboard.admin')) {
+                    abort(404);
+                }
+            });
 
-        //     View::composer(['corporate.index','corporate.posts.*'], function ($view) use($historial) {
-        //         if (\Shinobi::can('sponsor.quit.others') == false) {
-        //             $pay_sponsor = \App\SponsorPay::where('finish_date', '>' ,\Carbon\Carbon::now())
-        //                 ->where('prints','>','print_count')
-        //                 ->where('status','active')
-        //                 ->first();
-        //             if($pay_sponsor != null)
-        //             {
-        //                 $pay_sponsor->update(['print_count' => $pay_sponsor->print_count +1]);
-        //                 if($pay_sponsor->print_count == $pay_sponsor->prints )
-        //                     $pay_sponsor->update(['status' => 'finish']);
-        //                 \App\SponsorPrint::create([
-        //                     'sponsor_id' => $pay_sponsor->sponsor->id,
-        //                     'created_at' => \Carbon\Carbon::now(),
-        //                     'historial_id' => $historial->id,
-        //                 ]);
-        //                 $view->with('sponsor_show', $pay_sponsor->sponsor);
-        //             }
-        //         }
-        //     });
+            View::composer(['corporate.index','corporate.posts.*'], function ($view) use($historial) {
+                if (\Shinobi::can('sponsor.quit.others') == false) {
+                    $pay_sponsor = \App\SponsorPay::where('finish_date', '>' ,\Carbon\Carbon::now())
+                        ->where('prints','>','print_count')
+                        ->where('status','active')
+                        ->first();
+                    if($pay_sponsor != null)
+                    {
+                        $pay_sponsor->update(['print_count' => $pay_sponsor->print_count +1]);
+                        if($pay_sponsor->print_count == $pay_sponsor->prints )
+                            $pay_sponsor->update(['status' => 'finish']);
+                        \App\SponsorPrint::create([
+                            'sponsor_id' => $pay_sponsor->sponsor->id,
+                            'created_at' => \Carbon\Carbon::now(),
+                            'historial_id' => $historial->id,
+                        ]);
+                        $view->with('sponsor_show', $pay_sponsor->sponsor);
+                    }
+                }
+            });
 
-        //     View::composer(['corporate.posts.show','pdf.view'], function ($view) use($historial)  {
-        //         \App\PostHistorial::create([
-        //             'user_id' => (\Auth::user())? \Auth::user()->id: null,
-        //             'post_id' => $view->post->id,
-        //             'activity' => 'visit',
-        //             'details' => (\Auth::user())? 'Visita desde usuario': 'Visita sin usuario',
-        //             'historial_id' => $historial->id,
-        //         ]);
-        //         \App\PostVisit::create([
-        //             'post_id' => $view->post->id,
-        //             'user_id' => (\Auth::user())? \Auth::user()->id: null,
-        //             'historial_id' => $historial->id,
-        //             'created_at' => \Carbon\Carbon::now(),
-        //         ]);
-        //     });
+            View::composer(['corporate.posts.show','pdf.view'], function ($view) use($historial)  {
+                \App\PostHistorial::create([
+                    'user_id' => (\Auth::user())? \Auth::user()->id: null,
+                    'post_id' => $view->post->id,
+                    'activity' => 'visit',
+                    'details' => (\Auth::user())? 'Visita desde usuario': 'Visita sin usuario',
+                    'historial_id' => $historial->id,
+                ]);
+                \App\PostVisit::create([
+                    'post_id' => $view->post->id,
+                    'user_id' => (\Auth::user())? \Auth::user()->id: null,
+                    'historial_id' => $historial->id,
+                    'created_at' => \Carbon\Carbon::now(),
+                ]);
+            });
 
-        //     View::composer('pdf.view', function ($view) use($historial)  {
-        //         \App\PdfView::create([
-        //             'path_pdf' => $view->post->pdf,
-        //             'post_id' => $view->post->id,
-        //             'user_id' => \Auth::user()->id,
-        //             'created_at' => \Carbon\Carbon::now(),
-        //             'historial_id' => $historial->id,
-        //         ]);
-        //     });
+            View::composer('pdf.view', function ($view) use($historial)  {
+                \App\PdfView::create([
+                    'path_pdf' => $view->post->pdf,
+                    'post_id' => $view->post->id,
+                    'user_id' => \Auth::user()->id,
+                    'created_at' => \Carbon\Carbon::now(),
+                    'historial_id' => $historial->id,
+                ]);
+            });
             
-        // }
+        }
 
     }
 
--- resources/views/corporate/posts/show.blade.php
+++ resources/views/corporate/posts/show.blade.php
@@ -39,10 +39,33 @@
 		<div class="row">
 			<div class="col-sm-8">
 				
-				@include('corporate.sponsors.print',['print'=>'all'])
 				<?php
 					echo $post->body;
 				?>
+				<div class="comments-post">
+					<h3>Comentarios</h3>
+					<div id="disqus_thread"></div>
+					<script>
+
+					/**
+					*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+					*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+					/*
+					var disqus_config = function () {
+					this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+					this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+					};
+					*/
+					(function() { // DON'T EDIT BELOW THIS LINE
+					var d = document, s = d.createElement('script');
+					s.src = 'https://neurocodigo.disqus.com/embed.js';
+					s.setAttribute('data-timestamp', +new Date());
+					(d.head || d.body).appendChild(s);
+					})();
+					</script>
+					<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+					                            
+				</div>
 			</div>
 
 			<div class="col-sm-4">
@@ -61,6 +84,8 @@
 					</div>
 				@endforeach
 				<hr>
+				@include('corporate.sponsors.print',['print'=>'all'])
+				
 				@if(count($post->pdfs) > 0)
 					<div class="container-img-pdf">
 							@if(Auth::user() && Auth::user()->postStatus($post->id) /* && Shinobi::can('post.pdf.show')*/)
