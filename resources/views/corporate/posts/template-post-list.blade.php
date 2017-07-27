5d1eb250397d8efa79b426782dabe7a69f3d9c65
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2017-07-26 22:37:25 -0500

    modificar valores de la imagen principal se obtiene publicidad solo si hay publicidades existentes

--- resources/views/corporate/posts/template-post-list.blade.php
+++ resources/views/corporate/posts/template-post-list.blade.php
@@ -1,7 +1,7 @@
 		<div class="list-post">
 			@foreach($posts as $index =>$post)
 
-				@if(($index+1) % 5 ==0)
+				@if(($index==2 || ($index>9 && ($index+1) % 5 ==0)) && isset($sponsor_show))
 					<div class="preview-post preview-sponsor">
 						@include('corporate.sponsors.print')
 					</div>
