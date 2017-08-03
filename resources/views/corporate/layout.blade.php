f7925a1387986f68efde713478d421b5220d39d1
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2017-08-02 21:28:48 -0500

    Se quito los mensajes de error al momento de usar form

--- resources/views/corporate/layout.blade.php
+++ resources/views/corporate/layout.blade.php
@@ -100,13 +100,6 @@
 			</div>
 		@endif
 		
-		@if(\Session::has('errors'))
-			<div class="alert alert-danger alert-dismissible" role="alert">
-				<button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">×</span></button>
-				<i class="fa fa-times-circle"></i> {{ \Session::get('errors') }}
-			</div>
-		@endif
-		
 		@if(\Session::has('success'))
 			<div class="alert alert-success alert-dismissible" role="alert">
 				<button type="button" class="close" data-dismiss="alert" aria-label="Close">
