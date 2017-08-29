f4a39b0edaf6cd52b52ebf0b20f87902baa5314a
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2017-08-29 14:11:52 -0500

    total visitas en el dashboard restando las impresiones de los posts

--- app/SponsorHistorial.php
+++ app/SponsorHistorial.php
@@ -3,13 +3,10 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SponsorHistorial extends Model
 {
     //
-  use SoftDeletes;
-	protected $dates = ['deleted_at'];
   protected $fillable = [
   	'author_id','sponsor_id','activity','details',
   ];
