173538830b47878b56f6396323703f1e224d0b4d
Franklin Peñafiel <franklinpenafiel1991@gmail.com>
2017-09-10 11:30:01 -0500

    se cambio el diseño a mostrar a imagen de entrada y las publicaciones, servicios, buscador  y demas detalles

--- resources/views/flat/posts/list-index.blade.php
+++ resources/views/flat/posts/list-index.blade.php
@@ -1,59 +1,67 @@
-<h2>Servicios</h2>
-<div class="row">
-	
-	<div class="col-md-1">
-		<div class="btn-group">
-			<a class="btn btn-danger btn-xs" href="#scroller" data-slide="prev"><i class="fa fa-angle-left"></i></a>
-			<a class="btn btn-danger btn-xs" href="#scroller" data-slide="next"><i class="fa fa-angle-right"></i></a>
-		</div>
+<section id="services" class="">
+	<div>
+		<h2>Servicios</h2>
 	</div>
-	<div class="col-md-11">
-		<div id="scroller" class="carousel slide">
-			<div class="carousel-inner">
-				@foreach($categories as $index => $category)
-					@if($index == 0 || ($index%4)==0)
-						<div class="item @if($index == 0) active @endif">
-							<div class="row">
-					@endif
-								<div class="col-xs-3">
-									<div class="portfolio-item">
-										<div class="item-inner">
-											<img class="img-responsive" src="{{ url('storage/'.$category->image) }}" alt="">
-											<h5>
-												{{ $category->name }}
-											</h5>
-											<div class="overlay">
-												<a class="preview btn btn-danger" title="{{ $category->excerpt }}" href="{{ route('show-service',[$category->slug]) }}"><i class="fa fa-eye"></i></a>
+	<div class="row">
+		
+		<div class="col-md-1">
+			<div class="btn-group">
+				<a class="btn btn-danger btn-xs" href="#scroller" data-slide="prev"><i class="fa fa-angle-left"></i></a>
+				<a class="btn btn-danger btn-xs" href="#scroller" data-slide="next"><i class="fa fa-angle-right"></i></a>
+			</div>
+		</div>
+		<div class="col-md-11">
+			<div id="scroller" class="carousel slide">
+				<div class="carousel-inner">
+					@foreach($categories as $index => $category)
+						@if($index == 0 || ($index%4)==0)
+							<div class="item @if($index == 0) active @endif">
+								<div class="row">
+						@endif
+									<div class="col-md-3 col-xs-6">
+										<div class="portfolio-item">
+											<div class="item-inner">
+												<img class="img-responsive" src="{{ url('storage/'.$category->image) }}" alt="">
+												<h5>
+													{{ $category->name }}
+												</h5>
+												<div class="overlay">
+													<a class="preview btn btn-danger" title="{{ $category->excerpt }}" href="{{ route('show-service',[$category->slug]) }}"><i class="fa fa-eye"></i></a>
+												</div>
 											</div>
 										</div>
-									</div>
-								</div>   
+									</div>   
 
-					@if((($index+1)%4)==0)
-							</div><!--/.row-->
-						</div><!--/.item-->
-					@endif
-				@endforeach
+						@if((($index+1)%4)==0 || $index == count($categories)-1)
+								</div><!--/.row-->
+							</div><!--/.item-->
+						@endif
+					@endforeach
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
-<h2>Publicaciones</h2>
+</section>
+<hr>
 <section id="portfolio" class="container">
-	<ul class="portfolio-items col-3">
-		@foreach($posts as $post)
+	<h2>Publicaciones</h2>
+	<div class="row">
+		
+		<ul class="portfolio-items ">
+			@foreach($posts as $post)
 
-			<li class="portfolio-item">
-				<div class="item-inner">
-					<img src="{{ url('storage/'.$post->image) }}" alt="">
-					<h5>{{ $post->title }}</h5>
-					<h6><u>{{ $post->category->name }}</u></h6>
-					<div class="overlay">
-						<a class="preview btn btn-danger" href="{{ route('show-post',[$post->category->slug,$post->slug]) }}"><i class="fa fa-eye"></i></a>              
+				<li class="portfolio-item col-md-4 co-xs-12">
+					<div class="item-inner">
+						<img src="{{ url('storage/'.$post->image) }}" alt="">
+						<h5>{{ $post->title }}</h5>
+						<h6><u>{{ $post->category->name }}</u></h6>
+						<div class="overlay">
+							<a class="preview btn btn-danger" href="{{ route('show-post',[$post->category->slug,$post->slug]) }}"><i class="fa fa-eye"></i></a>              
+						</div>           
 					</div>           
-				</div>           
-			</li><!--/.portfolio-item-->
-		@endforeach
-	</ul>
+				</li><!--/.portfolio-item-->
+			@endforeach
+		</ul>
+	</div>
 	{{ $posts->links() }}
 </section><!--/#portfolio-->
